42458ff87ef91ced8b3eb505f8c7c9c0bc2807cd
sebas <esrbastianherrera@gmail.com>
2025-03-24 15:52:07 -0600

    feat: add main gem file

--- lib/rails_location_header.rb
+++ lib/rails_location_header.rb
@@ -1,8 +1,18 @@
 # frozen_string_literal: true
 
 require_relative "rails_location_header/version"
+require "created_entity_header/railtie" if defined?(Rails)
+require "created_entity_header/controller_extension"
 
 module RailsLocationHeader
-  class Error < StandardError; end
-  # Your code goes here...
+  class Middleware
+    def initialize(app)
+      @app = app
+    end
+
+    def call(env)
+      status, headers, response = @app.call(env)
+      [status, headers, response]
+    end
+  end
 end
